a6242197b6088febf6f700f083aa41309e774a3d
Frederik Folkvardsen <fbf@peytz.dk>
2024-03-12 14:38:18 +0100

    fix: cleanup in app.py after testing

---
+++ pyseidon/rewrite-constants.py
@@ -0,0 +1,17 @@
+import base64
+import json
+# print(base64.b64decode('DumlkrqalRjQWYbWVvQMIRTEmTwRuymTjSHUcwTNjm4=').hex())
+# print(base64.b64decode('APFEUjXyFIxZhlhxafwbzYh7CNTQCGjfVpb/9AlW6GQ=').hex())
+# data = {}
+# data["C"] = []
+# data["M"] = []
+# for i in range(len(C)):
+#     data["C"].append("0x"+base64.b64decode(C[i]).hex())
+# for x in range(len(M)):
+#     data["M"].append([])
+#     for y in range(len(M[x])):
+#         data["M"][x].append("0x"+base64.b64decode(M[x][y]).hex())
+
+
+# with open("pyseidon/2.json", "w") as file:
+#         json.dump(data, file, indent=2)
